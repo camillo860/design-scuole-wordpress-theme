7a5c96a0f741d54006f90bb29c0274ac118fe50b
camillo860 <camillo860@hotmail.com>
2023-06-05 15:51:15 +0200

    logo changes

--- template-parts/common/logo.php
+++ template-parts/common/logo.php
@@ -1,6 +1,4 @@
 <a href="<?php echo home_url(); ?>" aria-label="Vai alla homepage" title="Vai alla homepage">
-<?xml version="1.0" encoding="utf-8"?>
-<!-- Generator: Adobe Illustrator 25.0.1, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
 <svg version="1.1" id="Livello_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 	 viewBox="0 0 154 81" style="enable-background:new 0 0 154 81;" xml:space="preserve">
 <style type="text/css">
